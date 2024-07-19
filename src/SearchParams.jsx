28254316718c26c9e2c12dd49248f928b9aa1f1e
daracoo <dkuzmanoski21@gmail.com>
2024-07-19 16:45:32 +0200

    Custom useBreedList hook that determines which animal has been selected and provides a list of breeds based on that animal type.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Pet from "./Pet"
+import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -7,10 +8,11 @@ const SearchParams = () => {
     const [animal, setAnimal] = useState("");
     const [breed, setBreed] = useState("");
     const [pets, setPets] = useState([]);
+    const [breeds] = useBreedList(animal);
 
     useEffect(() => {
         requestPets();
-    }, []);
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     async function requestPets(){
         const res = await fetch(
@@ -21,8 +23,7 @@ const SearchParams = () => {
 
         setPets(json.pets);
     }
-
-    const breeds = [];
+    
     return(
         <div className="search-params">
             <form onSubmit={e => {
