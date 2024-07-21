d488f437220e83935e6a01c532ae957eb1d8251d
daracoo <dkuzmanoski21@gmail.com>
2024-07-21 21:15:24 +0200

    Fixed Errors in ErrorBoundary & Added Modal to Render on the Pet Details Page

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const Details = () => {
+    const [showModal, setShowModal] = useState(false);
     const {id} = useParams();
     const results = useQuery(["details", id], fetchPet);
 
@@ -23,11 +27,33 @@ const Details = () => {
             <div>
                 <h1>{pet.name}</h1>
                 <h2>{pet.animal} - {pet.breed} - {pet.city}, {pet.state}</h2>
-                <button>Adopt {pet.name}</button>
+                <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
                 <p>{pet.description}</p>
+                {
+                    showModal ?
+                    (
+                        <Modal>
+                            <div>
+                                <h1>Would you like to {pet.name}?</h1>
+                                <div className="buttons">
+                                    <button>Yes</button>
+                                    <button onClick={() => setShowModal(false)}>No</button>
+                                </div>
+                            </div>
+                        </Modal>
+                    ) : null
+                }
             </div>
         </div>
     )
   };
+
+  function DetailsErrorBoundary(props){
+    return (
+        <ErrorBoundary>
+            <Details {...props}/>
+        </ErrorBoundary>
+    )
+  }
   
-  export default Details;
+  export default DetailsErrorBoundary;
